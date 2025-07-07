3349957b7457dcac7026849207c0779c615b0254
josuechevez1998 <josuechevez977@gmail.com>
2025-07-07 15:19:38 -0600

    contenido estatico

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 const express = require('express');
-const dotenv = require('dotenv');
-
-// Cargar variables de entorno
-dotenv.config();
-
 const app = express();
+
 const port = process.env.APP_PORT || 3000;
+const host = process.env.APP_HOST || 'localhost';
 
+// Servir contenido estatico
+app.use(express.static('view'));
+app.use(express.static('public'));
 
-// Ruta de prueba
-app.get('/', (req, res) => {
-  res.send('¡Hola desde Docker con Node.js y Express GuanacoDev!');
+app.get('/index', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-app.listen(port, () => {
-  console.log(`🚀 Servidor escuchando en http://localhost:${port}`);
+// Middleware para manejar rutas no encontradas (404)
+app.use((req, res, next) => {
+  res.sendFile(__dirname + '/views/http-erros/404.html');
+});
+
+
+app.listen(port, e => {
+  console.log(`Corriendo en http://${host}:${port}`);
 });
